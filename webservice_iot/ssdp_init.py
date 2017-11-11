197c2d9306f0db5a4eb0fceed00730a745784325
gabriel.franzoni <gabriel.rolfsen@gmail.com>
2017-11-11 16:17:11 -0200

    * Add SSDP Init

---
+++ webservice_iot/ssdp_init.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python
+
+from lib.ssdp import SSDPServer
+from lib.upnp_http_server import UPNPHTTPServer
+import uuid
+import netifaces as ni
+from time import sleep
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+def get_network_interface_ip_address(interface='en0'):
+    """
+    Get the first IP address of a network interface.
+    :param interface: The name of the interface.
+    :return: The IP address.
+    """
+    while True:
+        if NETWORK_INTERFACE not in ni.interfaces():
+            logger.error('Could not find interface %s.' % (interface,))
+            exit(1)
+        interface = ni.ifaddresses(interface)
+        if (2 not in interface) or (len(interface[2]) == 0):
+            logger.warning('Could not find IP of interface %s. Sleeping.' % (interface,))
+            sleep(60)
+            continue
+        return interface[2][0]['addr']
+
+
+device_uuid = uuid.uuid4()
+
+NETWORK_INTERFACE = 'en0'
+local_ip_address = get_network_interface_ip_address(NETWORK_INTERFACE)
+
+ssdp = SSDPServer()
+ssdp.register('local',
+              'uuid:{}::upnp:rootdevice'.format(device_uuid),
+              'upnp:rootdevice',
+              'http://{}:5000'.format(local_ip_address))
+ssdp.run()
